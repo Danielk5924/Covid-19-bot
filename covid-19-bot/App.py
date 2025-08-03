83cc62ff95e861f435062d171d06e55cc25b5e4b
Daniel Konvicka <daniel.bob.dmk@gmail.com>
2025-08-03 14:29:26 -0700

    old model using old data

--- covid-19-bot/App.py
+++ covid-19-bot/App.py
@@ -1,34 +1,105 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, jsonify, redirect, url_for, request
+import torch
+from torch import nn
+from torch import optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset, Subset
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from flask_cors import CORS
 import pandas as pd
 import numpy as np
 import pickle as pickle 
+import os
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.route('/predict', methods = ["POST"])
+@app.route('/predict', methods=["POST"])
 def predict():
     answers = request.json
+    answers[0] = 1 if answers[0] == "female" else 2
+
     print(answers)
-
-    with open('finalizedmode.sav', 'rb') as f:
+    tensor_answers = torch.tensor(answers, dtype=torch.float32)
+    
+    with open('/Users/daniel/Programming/Covid-19-bot/covid-19-bot/model.pkl', 'rb') as f:
         loaded_model = pickle.load(f)
+    print("Model loaded successfully!")
+    loaded_model.eval()
+    with torch.no_grad():
+        predict = loaded_model(tensor_answers)
+    print(predict)
+    _, predicted_class = torch.max(predict, 0)
+    # ### DATA LOADING (without creating a new StandardScaler)
+    # testdata = pd.read_csv('Covid Data 2.csv').values
+    # print(testdata.head())
+    # unscaled_testdata = np.delete(testdata, [0,1], axis=1)
+    # ## Note there is no scaling here
+    check = {0:"no covid", 1:"mild", 2:"moderate", 3:"severe"}
+    predicted_value = check[predicted_class.item()]
+    
+    # prediction = loaded_model.predict(unscaled_testdata)
+    # print(prediction)
+    print("I AM WORKING WELL!")
+    print(predicted_value)
 
-    ### DATA LOADING (without creating a new StandardScaler)
-    testdata = pd.read_csv('Covid Data 2.csv').values
-    print(testdata.head())
-    unscaled_testdata = np.delete(testdata, [0,1], axis=1)
-    ## Note there is no scaling here
-
-    prediction = loaded_model.predict(unscaled_testdata)
-    print(prediction)
-    return prediction.tolist()
+    return jsonify(predicted_value)
 
 
         
 
 if __name__ == '__main__':
-    testdata = pd.read_csv('Covid Data 2.csv')
-    #print(testdata.head())
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    DATA_PATH = os.path.join(BASE_DIR, "public", "data.csv")
+    df = pd.read_csv(DATA_PATH)
+    #print(df.head())
+
+    df['DATE_DIED'] = [1 if each =="9999-99-99" else 0 for each in df["DATE_DIED"]]
+    df['CLASIFFICATION_FINAL'] = [0 if each > 3 else each for each in df['CLASIFFICATION_FINAL']]
+    print("HIIIIIII",df['CLASIFFICATION_FINAL'].value_counts())
+    df.drop('USMER', axis=1, inplace=True)
+    df.drop('MEDICAL_UNIT', axis=1, inplace=True)
+    df.drop('PATIENT_TYPE', axis=1, inplace=True)
+    #print(df.head())
+
+    x = df.iloc[0:400, list(range(0,16)) + [17]]
+    y = df['CLASIFFICATION_FINAL'].iloc[0:400]
+    x.to_numpy()
+    y.to_numpy()
+    x_test = df.iloc[400:800, list(range(0,16)) + [17]]
+    y_test = df['CLASIFFICATION_FINAL'].iloc[400:800]
+
+    scaler = StandardScaler()
+    scaler.fit(x)
+    x_scaled = scaler.transform(x)
+    scaler.fit(x_test)
+    x_test_scaled = scaler.transform(x_test)
+
+    class MultiLayerLogisticRegression(nn.Module):
+        def __init__(self, input_size, num_classes):
+            super(MultiLayerLogisticRegression, self).__init__()
+            self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, 64),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(p=0.3),
+            nn.Linear(64, num_classes)
+            )
+
+        def forward(self, x):
+            logits = self.linear_relu_stack(x)
+            return logits
+
+    app.run(debug=False, host='0.0.0.0', port=8000)
--- covid-19-bot/src/Question.js
+++ covid-19-bot/src/Question.js
@@ -3,41 +3,65 @@ import { useState } from "react";
 import './Question.css';
 
 function Question() {
-    const data = [{'Q':'Have you had tuberculosis!', 'O':["yes","no"]}, {"Q":"Are you pregnant?", "O":["yes","no"]}];
+    const data = [{'Q':'What is your sex?', 'O':[1, 2]}, 
+    {"Q":"are you dead?", "O":[0,1]},
+    {'Q':'are you connected to a ventilator?', 'O':[1,2]},
+    {'Q':'Do you have pneumonia?', 'O':[1,2]},
+    {'Q':'How old are you?', 'O':[50, 60, 70, 80, 90]},
+    {'Q':'are you pregnant?', 'O':[1,2]},
+    {'Q':'Do you have diabetes?', 'O':[1,2]},
+    {'Q':'Do you have Copd', 'O':[1,2]},
+    {'Q':'Do you have asthma?', 'O':[1,2]},
+    {'Q':'Are you immuno suppressed?', 'O':[1,2]},
+    {'Q':'do you have hypertension?', 'O':[1,2]},
+    {'Q':'Any other diseases?', 'O':[1,2]},
+    {'Q':'Do you have a any heard or blood realted diseases?', 'O':[1,2]},
+    {'Q':'Are you obese?', 'O':[1, 2]},
+    {'Q':'Do you have chronic renal disease', 'O':[1,2]},
+    {'Q':'do you use tobacco?', 'O':[1,2]},
+    {'Q':'Have you been admitted to ICU?', 'O':[1,2]}];
+
     const [QuestionIndex, setQuestionIndex] = useState(0);
     const [value, setValue] = useState('');
     const [answers, setAnswers] = useState([]);
-    console.log(answers);
     const [num, addNum] = useState(0);
-    const {diagnosis, setDiagnosis} = useState(0);
+    const [diagnosis, setDiagnosis] = useState(0);
 
     function handleChange(event){
+        setAnswers([...answers, value]);
+        addNum(num + 1);
+        setQuestionIndex(QuestionIndex + 1);
         setValue(event.target.value); 
       };
 
-    function handleSubmit(){
-        const data = { 'answers': answers };
-        axios.post('http://localhost:8080/predict', data)
+    function handleSubmit(){         
+        addNum(num+1);                                                                                                            
+        const data = [...answers, value];
+        data.forEach((val, index) => {data[index] = parseFloat(val)});
+        console.log(data);
+        axios.post('http://localhost:8000/predict', data, { headers: {'Content-Type': 'application/json'}})
         .then(response => setDiagnosis(response.data));
-
+        setValue('');
     }
 
     
     return(
-     <div className = "question-container">
+    <div>
+    {num < 17 && <div className = "question-container">
          <div className = "big-quesiton" style={{ fontSize: '100px' }} >  
              {data[QuestionIndex]['Q']}
          </div>
 
          <div>
-             <select className="big-select" value = {value} onChange ={handleChange}>
-            
+             <select className="big-select" value = {value} onChange ={(event) => setValue(event.target.value)}>
+            <option value="" disabled hidden>
+                Select an option...
+            </option>
              {data[QuestionIndex]["O"].map((option) => (
-            
              <option key={option} value={option}>
                  {option}
              </option>
-                ))}
+                ))} 
 
              </select>
              <p> {value}</p>
@@ -46,28 +70,28 @@ function Question() {
         
         
 
-         <div>
-             <button onClick={() => {setQuestionIndex(QuestionIndex+1) 
-                setAnswers([...answers, value])
-                addNum(num + 1)}} className = "big-button">
+         {num !== 16 && <div>
+             <button onClick={handleChange} 
+                className = "big-button"
+                disabled={value === ""}>
                  next
              </button>
-         </div>
-         {num === 1 &&<div> 
-            <button onClick = {() => handleSubmit()}>
+         </div>}
+         {num === 16 &&<div> 
+            <button onClick = {handleSubmit}
+                className = "big-button"
+                disabled={value === ""}>
                 submit
             </button>
+         </div> 
+         }
          </div>}
-
-         <div>
-            if (diagnosis === 0){
-
-            }else{
-
-                
-            }
+         
+        <div>
+            diagnosis: {diagnosis}
          </div>
-     </div>
+
+         </div>
 
     );
 
